9aaf675d5638e25d8c93de47e18f40e146283ee7
Manurbhav Arya <110805836+mannubhai1@users.noreply.github.com>
2024-01-27 14:19:53 +0530

    finished

---
+++ index.js
@@ -0,0 +1,59 @@
+var number = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < number; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonText = this.innerHTML;
+
+    makeSound(buttonText);
+
+    addAnimations(buttonText);
+
+    });
+}
+
+document.addEventListener("keypress", function (event) {
+
+    makeSound(event.key);
+    addAnimations(event.key);
+
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            var kick_bass = new Audio("sounds/kick-bass.mp3");
+            kick_bass.play();
+            break;
+        case "l":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        default:
+            console.log(this.innerHTML);
+    }
+}
+
+function addAnimations(currentKey){
+    document.getElementsByClassName(currentKey).classList.toggle("pressed");
+}
